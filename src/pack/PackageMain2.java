9442575b36b0ea5469a45b6fab237fa2e8eaa58c
indexing02 <indexing02@gmail.com>
2025-01-13 00:57:43 +0900

    0112 패키지

---
+++ src/pack/PackageMain2.java
@@ -0,0 +1,17 @@
+package pack;
+//import pack.a.User;
+//import pack.a.User2;
+import pack.a.*;
+
+public class PackageMain2 {
+    public static void main(String[] args) {
+        Data data = new Data();
+        User user = new User();
+        User2 user2 = new User2();
+}
+
+    //코드 첫줄에는 package를 사용, 다음줄에는 import를 사용할 수 있음
+    //import를 사용하면 다른 패키지에 있는 클래스를 가져와서 사용할 수 있음
+    //->코드에서 패키지 명을 생략하고 클래스 이름만 적을 수 있음
+
+}
